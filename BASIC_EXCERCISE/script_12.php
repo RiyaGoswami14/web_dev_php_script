e93a68c8844b7a470250f2ab22499d8aa9e14360
RiyaGoswami14 <143240403+RiyaGoswami14@users.noreply.github.com>
2023-09-19 18:03:41 +0530

    Add files via upload

---
+++ BASIC_EXCERCISE/script_12.php
@@ -0,0 +1,9 @@
+<script>
+	
+</script>
+<?php
+echo "<ul><li><font color=red>Riya</font></li>
+		  <li><font color=blue>Urvi</font></font></li>
+		  <li><font color=pink>Minal</font></li>
+	  </ul>";
+?>
