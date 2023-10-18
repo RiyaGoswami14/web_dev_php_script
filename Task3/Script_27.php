f3501cd02e11b90b39fba7fd0b557a49aa9ffea2
RiyaGoswami14 <143240403+RiyaGoswami14@users.noreply.github.com>
2023-10-18 12:36:19 +0530

    Add files via upload

---
+++ Task3/Script_27.php
@@ -0,0 +1,6 @@
+<?php
+//create a php script to calculateand display the lengthof an array using then count()function.
+$subjects=array("php","html","dbms","javascript","c","python");
+echo count($subjects);
+
+?>
