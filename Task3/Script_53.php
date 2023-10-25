439ca30d37ce3859ff38f472f1b4546ae72bac1c
RiyaGoswami14 <143240403+RiyaGoswami14@users.noreply.github.com>
2023-10-26 01:02:29 +0530

    Add files via upload

---
+++ Task3/Script_53.php
@@ -0,0 +1,5 @@
+<?php
+$a=array("black","green","black","yellow");
+print_r(array_unique($a));
+
+?>
---
+++ Task3/Script_55.php
@@ -0,0 +1,14 @@
+<?php
+$array = ['fruit' => 'apple', 'flower' => 'black rose', 'food' => 'biryani'];
+
+$firstKey = array_key_first($array);
+
+print_r($firstKey);
+
+$lastkey=array_key_last($array);
+
+echo"</br>";
+print_r($lastkey);
+
+
+?>
